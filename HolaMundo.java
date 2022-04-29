05d5c40f52e6d83d8327f942fff8b15d08f6cca4
bryanhancco <bhanccoco@unsa.edu.pe>
2022-04-28 21:15:11 -0500

    new

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,7 +1,8 @@
 public class HolaMundo {
 	public static void main(String args[]) {
 		System.out.println("Hola mundo");
-		System.out.println("Hola mundo";
-		System.out.println("Hola mundo";
+	}
+	public int suma(int a, int b){
+		return a+b;
 	}
 }
